0cca8f76c29b8ea0ed1b2810173e8cf331389596
meduduna Akerejola <cremmybanj7@gmail.com>
2022-04-28 08:57:16 +0100

    finished market place solidity code with adequate comments

--- src/contracts/MarketPlace.sol
+++ src/contracts/MarketPlace.sol
@@ -4,8 +4,12 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./Token.sol";
 
 contract MarketPlace{
-    // an array of nfts for sale on the marketplace
-    Item[] itemsForSale;
+    // total number of uploaded items
+    uint itemsCount;
+    // a mapping of number of items to id of the items
+    mapping(uint=>uint) itemsForSale;
+    // a mapping of id of items to the item
+    mapping(uint=>Item) itemId_Item;
     // variables modified by deployer
     address payable feeBank;
     uint percentage;
@@ -15,6 +19,7 @@ contract MarketPlace{
         uint id;
         address creator;
         uint price;
+        bool sold;
     }
     // event emitted when seller uploads nft to marketplace
     event Uploaded(address market,uint price,uint itemId);
@@ -29,14 +34,46 @@ contract MarketPlace{
         require(price>=0,"Enter a valid price");
         // transfer ownership to the marketplace needs the approve function called
         nft.transferFrom(msg.sender,address(this),itemId);
-        // appends to the array of items for sale
-        itemsForSale.push(Item({
+        // increase the item count
+        itemsCount++;
+        // map the item count to its id
+        itemsForSale[itemsCount]=itemId;
+        // map the id to the item
+        itemId_Item[itemId]=Item({
             item:nft,
             id:itemId,
             creator:msg.sender,
             price:price
-        }));
+            sold:false
+        });
         // emit the uploaded event
         emit Uploaded(address(this),price,itemId);
+    }
+    // buyer buys nft
+    function buyNFT(uint itemNumber)public payable{
+        // get the item object using the number
+        Item item=itemId_Item[itemsForSale[itemNumber]];
+        // get the total fee (price of the item + market fee)
+        uint totalPrice=calc_totalFee(itemNumber);
+        // item must be among those listed on the marketplace
+        require(itemNumber>0 && itemNumber<=itemsCount,'Please enter a valid id');
+        // item should not have been sold
+        require (!(item.sold),'This item is not available for sale');
+        // buyer must have enough ether to cover both price and market fee
+        require (msg.value>totalPrice,'You do not have enough ether to purchase this item');
+        // transfer price to creator of the nft
+        item.creator.transfer(item.price);
+        // transfer market fee
+        feeBank.transfer(totalPrice-item.price);
+        // update the sold attribute
+        item.sold=true;
+        // finally transfer the nft from the marketplace to the buyer
+        item.nft.transferFrom(address(this),msg.sender,item.id)
+
+    }
+    // calculate the total fee for buyer
+    function calc_totalFee(uint itemNumber)returns(uint){
+        uint amount=itemId_Item[itemsForSale[itemNumber]].price;
+        return (amount+(amount*percentage/100))
     }
 }
